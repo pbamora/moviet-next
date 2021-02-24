d3c31b6fcb2e0b20ce582a380ef175345e956bed
Pedro Amora <pedro.amora@berrytech.com.br>
2021-02-24 17:44:46 -0300

    countDown init

--- src/Components/CountDown.tsx
+++ src/Components/CountDown.tsx
@@ -1,17 +1,44 @@
+import { useEffect, useState } from "react";
 import styles from "../styles/components/CountDown.module.css";
 
 export function CountDown() {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    if (!active || time === 0) return;
+    setTimeout(() => {
+      setTime(time - 1);
+    }, 1000);
+  }, [active, time]);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
+
   return (
-    <div className={styles.countDownContainer}>
-      <div>
-        <span>2</span>
-        <span>5</span>
+    <div>
+      <div className={styles.countDownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
       </div>
-      <span>:</span>
-      <div>
-        <span>2</span>
-        <span>5</span>
-      </div>
+
+      <button
+        onClick={() => setActive(!active)}
+        type="button"
+        className={styles.cowntDownButton}
+      >
+        {active === true ? "Encerrar Ciclo" : "Ativar Ciclo"}
+      </button>
     </div>
   );
 }
